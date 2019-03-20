5b1ce27fe8e412f8bcdca5f01acb888708043d84
SergeiGerbov <lyziifer_rab@mail.ru>
2019-03-20 17:12:58 +0300

    1.3. JavaDoc. Code convention[#122345]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,10 @@
 package ru.job4j.calculate;
-
+	/** 
+	* Вывод строки на консоль.
+	* Main
+	* @param args - args
+	* @author - Sergei Gerbov
+	/*
 public class Calculate {
 	
 	public static void main(String[] args) {
