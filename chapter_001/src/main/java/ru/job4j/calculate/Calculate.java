a312e43dbc51f892f37c2af5cc4196f87506f919
SergeiGerbov <lyziifer_rab@mail.ru>
2019-03-20 21:43:20 +0300

    1.4. JUnit[#122346]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,20 +1,29 @@
 package ru.job4j.calculate;
-/** 
-* Вывод строки на консоль.
-*@author - Sergei Gerbov.
-	*/
 
-
+/**
+ * Calculate.
+ *
+ * @author Sergei Gerbov
+ * @version $Id$
+ * @since 0.1
+ */
 public class Calculate {
-	
-	/**
-	*main.
-	*@param args - args.
-		*/
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
-}
+    /**
+     * Main.
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 
+    /**
+     * Method echo.
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Eсho, echo, echo : " + name;
+    }
+}
 	
 	
