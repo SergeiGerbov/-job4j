392c0c9b56d9ff5dc40ae1aebec75ae2fb16d33f
SergeiGerbov <lyziifer_rab@mail.ru>
2019-04-14 11:37:12 +0300

    Пазлы. Тестовое задание.[#122351]

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -2,10 +2,10 @@ package ru.job4j.array;
 
 public class MatrixCheck {
     public boolean mono(boolean[][] data) {
-        boolean result = false;
+        boolean result = true;
         for (int i = 0; i < data.length; i++) {
             if (data[0][0] != data[i][i] || (data[0][data.length - 1] != data[i][data.length - 1 - i])) {
-                result = true;
+                result = false;
                 break;
             }
         }
--- chapter_001/src/test/java/ru/job4j/array/MatrixCheckTest.java
+++ chapter_001/src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -28,11 +28,13 @@ public class MatrixCheckTest {
         };
         boolean result = check.mono(input);
         assertThat(result, is(false));
-    } @Test
+    }
+
+    @Test
     public void whenDataNotMonoByTrueThenFalse1() {
         MatrixCheck check = new MatrixCheck();
         boolean[][] input = new boolean[][]{
-                { true, false},
+                {true, false},
                 {true, true},
         };
         boolean result = check.mono(input);
